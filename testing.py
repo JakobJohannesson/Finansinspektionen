cbd94ec5b920cdc5f5866ecaf8568f309f93297b
JakobJohannesson <JakobJohannesson96@gmail.com>
2022-05-15 17:45:52 +0200

    Initial commit

---
+++ testing.py
@@ -0,0 +1,11 @@
+
+# python3 -m venv env && source ./env/bin/activate
+
+# Fungerar kanske inte direkt
+# source ./finansinstpektionen/bin/activate 
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+
